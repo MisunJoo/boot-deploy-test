a237f91f07188d4352a2fe5aaa066dc45db92276
MisunJoo <0987someday@naver.com>
2019-07-16 21:10:20 +0900

    add: hello world controller

---
+++ src/main/java/com/example/dolpintest/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.dolpintest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
+public class HelloWorldController {
+
+  @GetMapping("/")
+  public String getCurrentUser() {
+    return "hello world";
+  }
+}
+
---
+++ src/test/java/com/example/dolpintest/DolpinTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.dolpintest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DolpinTestApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
